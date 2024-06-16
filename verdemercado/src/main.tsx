61dd39b34b9326046ff9bf6a9325e4dace347a75
Matheus Ferreira Mendes <matheusmendesbmf@outlook.com>
2024-06-16 19:25:31 -0300

    index

--- verdemercado/src/main.tsx
+++ verdemercado/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.ts';
+import './index.css';
+import Index from './pages/index.tsx'; // Correcting the import statement
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Index /> {/* Rendering the Index component */}
+  </React.StrictMode>
+);
---
+++ verdemercado/src/pages/index.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+
+export default function Index() {
+  return (
+    <>
+      <header className="header-bg">
+        <div className="header">
+          <a href="./index.html"><img src="assets/img/Group 1.png" alt="Mercado Verde" /></a>
+          <nav aria-label="primaria">
+            <ul className="header-menu">
+              <li><a href="./index.html">HOME</a></li>
+              <li><a href="./mercado.html">MERCADO</a></li>
+              <li><a href="./minha_conta.html">MINHA CONTA</a></li>
+              <li><a href="./login.html"><img src="assets/img/Vector.svg" alt="Entrar" /></a></li>
+              <li><a href="pagamento.html"><img src="assets/img/Group 2.png" alt="Carrinho de Compra" /></a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <div className="carrossel">
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src="assets/img/Igor carrossel_1.png" alt="Primeiro Slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="assets/img/Igor carrossel_2.png" alt="Segundo Slide" />
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="assets/img/Igor carrossel_3.png" alt="Terceiro Slide" />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Anterior</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Próximo</span>
+          </a>
+        </div>
+      </div>
+
+      <h1><span>vamos</span> começar ;</h1>
+      <p className="texto1">Publique seu lance como um comprador registrado ou crie sua oferta de safra como um vendedor verificado pela plataforma.</p>
+
+      <div className="venda-geral">
+        <div className="vendas">
+          <img src="assets/img/Group 7.png" alt="inciar compra" />
+          <p className="linha1">Comunique ao mercado o que você está procurando e obtenha reações rápidas de agricultores interessados.</p>
+          <br />
+          <a className="compras" href="./mercado.html"><button type="submit">QUERO COMPRAR</button></a>
+        </div>
+        <div className="vendas">
+          <img src="assets/img/Group 8.png" alt="inciar venda" />
+          <p className="linha1">Publique ofertas para a safra que deseja vender e obtenha acesso imediato a compradores com crédito verificado.</p>
+          <br />
+          <a className="compras" href="./mercado.html"><button type="submit">QUERO VENDER</button></a>
+        </div>
+      </div>
+
+      <div className="banner2">
+        <img src="assets/img/Group 6.png" alt="Preços e Valores" />
+      </div>
+      <div className="tutorial-titulo">
+        <h2>Como funciona?</h2>
+        <h3>Acompanhe nosso passo-a-passo e assista ao nosso vídeo demonstrativo:</h3>
+      </div>
+      <br />
+      <br />
+      <div className="tutorial">
+        <div className="tutorial-lista">
+          <ol>
+            <li>Inscreva-se na plataforma</li>
+            <li>Crie sua oferta ou lance</li>
+            <li>Defina uma quantidade e preço</li>
+            <li>Forneça-nos características e detalhes da cultura</li>
+            <li>Publique sua oferta de vendedor ou lance de comprador</li>
+            <li>Mantenha-se atualizado por e-mail ou central de notificações</li>
+          </ol>
+        </div>
+        <div className="tutorial-img">
+          <iframe width="500" height="290" src="https://www.youtube.com/embed/q8desBqlXcA?controls=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      </div>
+      <div className="suporte">
+        <a href="mailto:mercado_verde@mercadoverde.com"><button className="suporte1" type="submit"><img src="assets/img/Group 20.png" alt="SUPORTE" /></button></a>
+      </div>
+      <br />
+      <br />
+      <br />
+      <footer className="footer-bg">
+        <div className="footer">
+          <div>
+            <ul className="lista-footer">
+              <li><b>MENU</b></li>
+              <br />
+              <li><a className="menu" href="./index.html">Home</a></li>
+              <li><a className="menu" href="./mercado.html">Mercado</a></li>
+            </ul>
+          </div>
+          <div>
+            <ul className="lista-footer">
+              <li><b>SOBRE NÓS</b></li>
+              <br />
+              <li>Quem Somos?</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+          <div>
+            <ul className="lista-footer">
+              <li><b>PRIVACIDADE</b></li>
+              <br />
+              <li>Política de Privacidade</li>
+              <li>Termos & Condições</li>
+            </ul>
+          </div>
+          <img src="assets/img/Line 2.png" alt="" />
+          <div>
+            <ul className="lista-footer">
+              <li><b>CONTATO</b></li>
+              <br />
+              <li><img src="assets/img/Vector1.png" alt="" /> (81) 99999-9999</li>
+              <li><img src="assets/img/Vector2.png" alt="" /> mercado_verde@mercadoverde.com</li>
+            </ul>
+          </div>
+          <div>
+            <img className="lista-footer" src="assets/img/Group 19.png" alt="" />
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
